acea8a1e9484e61509d9b85f1fe87d7e182d81bc
Taylor Vance <tmax8908@yahoo.com>
2025-05-09 15:13:22 -0500

    TTT show game result overlay

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -11,21 +11,19 @@ interface TextInputProps {
   max?: string;
 }
 
-const TextInput: React.FC<TextInputProps> = ({label, type='text', value, onChange, step, min, max}) => {
-  return (
-    <label className="flex flex-col gap-1">
-      {label}
-      <input
-        className="text-lg px-2 py-1 border rounded-lg border-gray-400 hover:border-gray-800"
-        type={type}
-        value={value}
-        onChange={onChange}
-        min={min}
-        max={max}
-        step={step}
-      />
-    </label>
-  );
-};
+const TextInput: React.FC<TextInputProps> = ({label, type='text', value, onChange, step, min, max}) => (
+  <label className="flex flex-col gap-0.5">
+    {label}
+    <input
+      className="text-lg px-2 py-1 border rounded-lg border-gray-400 hover:border-gray-800"
+      type={type}
+      value={value}
+      onChange={onChange}
+      min={min}
+      max={max}
+      step={step}
+    />
+  </label>
+);
 
 export default TextInput;
--- src/games/TicTacToe.tsx
+++ src/games/TicTacToe.tsx
@@ -2,7 +2,7 @@
 import TicTacToeState from 'multimcts/tictactoe';
 import { Game } from '../types/Game';
 import { GameState } from 'multimcts';
-import { FaX, FaO } from "react-icons/fa6";
+import { FaX, FaO, FaCat } from "react-icons/fa6";
 
 // Type guard to check if state is TicTacToeState
 const isTicTacToeState = (state: GameState): state is TicTacToeState => {
@@ -14,16 +14,31 @@ const render = (state:GameState, onMove:(move:string) => void) => {
     throw new Error("Invalid state type");
   }
 
+  const isTerminal = state.isTerminal();
+  let result = undefined;
+  if (isTerminal) {
+    if (state.getReward('moot') === 0) {
+      result = 'c';
+    } else {
+      result = state.team ? 'o' : 'x';
+    }
+  }
+
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div className="grid grid-cols-3 gap-2 relative">
       {state.board.map((cell, index) => (
         <button
           key={index}
           className={`w-20 h-20 bg-gray-200 flex items-center justify-center text-6xl ${cell===null ? '' : (cell ? 'text-red-600' : 'text-blue-600')}`}
           onClick={() => onMove(index.toString())}
-          disabled={cell!==null || state.isTerminal()}
+          disabled={cell!==null || isTerminal}
         >{cell!==null && (cell ? <FaX /> : <FaO />)}</button>
       ))}
+      {result && (
+        <div className="absolute inset-0 flex items-center justify-center opacity-50" style={{fontSize:'15em'}}>
+          {result==='c' ? <FaCat /> : (result==='x' ? <FaX className="text-red-600" /> : <FaO className="text-blue-600" />)}
+        </div>
+      )}
     </div>
   );
 };
